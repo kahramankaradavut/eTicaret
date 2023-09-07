1bd0a21c09a92a99872e0c874b6b50a08c6ef1ff
Kahraman Karadavut <kkaradavut.3@gmail.com>
2023-09-07 16:30:36 +0300

    1

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -18,7 +18,7 @@ class HomeController extends Controller
 
     /**
      * Show the application dashboard.
-     *a
+     *
      * @return \Illuminate\Contracts\Support\Renderable
      */
     public function index()
