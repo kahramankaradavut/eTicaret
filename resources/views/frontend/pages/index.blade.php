8451cdd4cbac8fb798201fbba9fa5903a1389380
Kahraman Karadavut <kkaradavut.3@gmail.com>
2023-09-22 22:01:48 +0300

    logo

--- resources/views/frontend/pages/index.blade.php
+++ resources/views/frontend/pages/index.blade.php
@@ -3,10 +3,10 @@
 @section('content')
 {{-- @dd($slider->image) --}}
 {{-- <div class="site-blocks-cover" style="background-image: url('{{ asset($slider->image ?? '') }}');" data-aos="fade"> --}}
-  <div class="site-blocks-cover" style="background-image: url({{'images/hero_1.jpg'}} );" data-aos="fade">
+  <div class="site-blocks-cover" style="background-image: url({{'images/cvfashionlogo.png'}} );" data-aos="fade">
     <div class="container">
         <div class="row align-items-start align-items-md-center justify-content-end">
-          <div class="col-md-5 text-center text-md-left pt-5 pt-md-0">
+          <div class="col-sm-3 text-center pt-5 pt-md-0">
             <h1 class="mb-2">{{$slider->name ?? __('Merhaba')}}</h1>
             <div class="intro-text text-center text-md-left">
               <p class="mb-4">{{$slider->content ?? ''}} </p>
